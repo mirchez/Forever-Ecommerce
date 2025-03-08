d7af63abb7fc4edc6895c99e04dff97b651f1e32
Miguel <mmirandasanchez16@gmail.com>
2025-03-08 14:29:27 -0300

    add new proyect

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {
+  Home,
+  Collection,
+  Contact,
+  Product,
+  Cart,
+  Login,
+  PlaceOrder,
+  Orders,
+  About,
+} from "./pages";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
