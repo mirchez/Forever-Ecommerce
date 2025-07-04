6f8843221c7f0915e9fb392c41131623b15ee562
Miguel <mmirandasanchez16@gmail.com>
2025-07-04 08:34:10 -0300

    solve page title bug

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ const Content = () => {
   return (
     <div>
       <div className="text-center text-2xl pt-10 botder-t">
-        <Title />
+        <Title text1="CONTACT" text2="US" />
       </div>
 
       <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
